430d57863017966641aa9d8803c7e347ec4fc4f0
louison courtois <louison.courtois@orange.fr>
2026-01-10 16:01:53 +0100

    :sparkles: add readme + flashCard router

--- src/routers/flashCardRouter.js
+++ src/routers/flashCardRouter.js
@@ -1,4 +1,37 @@
+import { Router } from "express";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
+import {
+  getAllQuestions,
+  createQuestions,
+  getQuestion,
+  deleteQuestion
+} from "../controllers/questionController.js";
 
+const router = Router();
 
-export default {}
+/**
+ * Lister toutes les questions
+ * GET /questions
+ */
+router.get("/", authMiddleware, getAllQuestions);
+
+/**
+ * Créer une question
+ * POST /questions
+ */
+router.post("/", authMiddleware, createQuestions);
+
+/**
+ * Récupérer une question
+ * GET /questions/:id
+ */
+router.get("/:id", authMiddleware, getQuestion);
+
+/**
+ * Supprimer une question
+ * DELETE /questions/:id
+ */
+router.delete("/:id", authMiddleware, deleteQuestion);
+
+export default router;
